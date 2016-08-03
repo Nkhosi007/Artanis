60d0d2d85ac02e148a7eeb0d592ba2bd1f8acd5a
Jerry Wong <jerrywong41@gmail.com>
2016-08-02 20:48:17 -0400

    issue1 fix2

--- portfolio.m
+++ portfolio.m
@@ -10,9 +10,10 @@ classdef portfolio < handle
         VIX = 22.7967654004844;
         excutedOrders = [];
         pendingOrders = [];
-        netPosition = zeros(0,9);
+        netPosition = zeros(0,10);
         cash=0;
-        netWorth;
+        portfolioMarketValue=0;
+        netWorth=0;
         status = [];
         orders = [];
         orderType = [0,0,1];
@@ -48,6 +49,22 @@ classdef portfolio < handle
             self.currentDay = unique(self.mktInfo(:,1));
             self.pendingOrders = [];
         end
+        
+        function markToMarket(self)
+           
+            for i = 1:size(self.netPosition,1)
+                pointer = self.netPosition(i,[1,2,3]);
+                head = ismember(self.mktInfo(:,[2,3,4]),pointer,'rows');
+                
+                excutePrices = [self.mktInfo(head,[6,7]);self.mktInfo(head,[7,6]);self.mktInfo(head,[8,8])];
+                longshort = sufficientStat(-self.netPosition(i,4));
+                ordersTypeVec = self.orderType;
+                self.netPosition(i,10) = -longshort*excutePrices'*ordersTypeVec';
+            end
+            
+            self.portfolioMarketValue = sum(self.netPosition(:,10));
+            self.netWorth = self.cash + self.portfolioMarketValue;
+        end
             
         function policy(self)
             
@@ -57,14 +74,14 @@ classdef portfolio < handle
                                        % Date  Put/Call  K    Exp   XX   bid   ask   mid   IV    Delta   F   DTE  Long Short limitOrder mktOrder midOrder GrossFlow Fee NetFlow   /end
                                        % 1       2      3      4     5    6     7     8     9    10     11    12   13   14       15        16       17        18     19   20      /end    
        % sample: self.activeOrders = [736270,    1,    215, 736344, 999, 2.01, 2.07, 2.04, 999,  999,   999,  74,  1,    0,       0,        0,       1];
-                self.activeOrders = [self.oldMktInfo,repmat([0,-1],2,1),repmat(self.orderType,2,1)]; %and order for test-purpose
+                self.activeOrders = [self.mktInfo,repmat([0,-1],2,1),repmat(self.orderType,2,1)]; %and order for test-purpose
                 
                 self.activeOrders = cat(2,self.activeOrders,zeros(size(self.activeOrders,1),3));
                 for i  = 1:size(self.activeOrders,1)
                     excutePrices = [self.activeOrders(i,[6,7]);self.activeOrders(i,[7,6]);self.activeOrders(i,[8,8])];
                     longshort = self.activeOrders(i,[13,14]);
-                    ordersType = self.activeOrders(i,[15,16,17]);
-                    gross = longshort*excutePrices'*ordersType';
+                    ordersTypeVec = self.activeOrders(i,[15,16,17]);
+                    gross = -longshort*excutePrices'*ordersTypeVec';
                     fee = abs(gross)*self.feeRate+self.fixFee;
                     net = gross - fee;
                     self.activeOrders(i,18:20) = [gross,fee,net];
@@ -85,8 +102,8 @@ classdef portfolio < handle
             for i  = 1:size(self.expiredOrders,1)
                     excutePrices = [self.expiredOrders(i,[6,7]);self.expiredOrders(i,[7,6]);self.expiredOrders(i,[8,8])];
                     longshort = self.expiredOrders(i,[13,14]);
-                    ordersType = self.expiredOrders(i,[15,16,17]);
-                    gross = longshort*excutePrices'*ordersType';
+                    ordersTypeVec = self.expiredOrders(i,[15,16,17]);
+                    gross = -longshort*excutePrices'*ordersTypeVec';
                     fee = abs(gross)*self.feeRate+self.fixFee;
                     net = gross - fee;
                     self.expiredOrders(i,18:20) = [gross,fee,net];
@@ -118,7 +135,7 @@ classdef portfolio < handle
                     self.netPosition(head,5) = impliedVol;
                     self.netPosition(head,6:9) = self.netPosition(head,6:9)+[weightedDelta,GFNIncrement];
                 elseif sum(head) == 0
-                    self.netPosition = cat(1,self.netPosition,[pointer,positionIncrement,impliedVol,weightedDelta,GFNIncrement]);
+                    self.netPosition = cat(1,self.netPosition,[pointer,positionIncrement,impliedVol,weightedDelta,GFNIncrement,0]);
                 else
                     error('check pointer');
                 end
@@ -127,9 +144,11 @@ classdef portfolio < handle
             %netPosition: Put/Call  K  Exp  position  IV  Delta  Gross  Fee  Net
             
             self.excutedOrders = cat(1,self.excutedOrders,self.pendingOrders);
+            self.cash = self.cash + sum(self.pendingOrders(:,20));
             self.pendingOrders = zeros(0,20);
-            self.cash = self.cash + sum(self.netPosition(self.netPosition(:,4)==0,9));
             self.netPosition = self.netPosition(self.netPosition(:,4)~=0,:);
+            self.markToMarket();
+            self.netWorth = self.cash + self.portfolioMarketValue;
             
         end
 
